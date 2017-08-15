0845ff29f104164d01c6bb733dda11f20ad38993
MHC User <mhcuser@n21-53.mtholyoke.edu>
2017-08-15 15:45:09 -0400

    homepage

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-  //example code - feel free to delete!
-  var timesSillyButtonClicked = 0;
-  $('#silly-button').click(function() {
-    timesSillyButtonClicked++;
-    $('#silly-button-text').text(
-      'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
-    );
-  });
+// $(document).ready(function() {
+//   //example code - feel free to delete!
+//   var timesSillyButtonClicked = 0;
+//   $('#silly-button').click(function() {
+//     timesSillyButtonClicked++;
+//     $('#silly-button-text').text(
+//       'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
+//     );
+//   });
+(confirm("I will agree to the I Can Pledge as shown below") === true)
 
-  //write your code here!
-});
+
+
+
+
+
+//
+//   //write your code here!
+// });
