1c02a0129bbb7642e6670cb560a8f0b5bb3cff0d
MHC User <mhcuser@n21-62.mtholyoke.edu>
2017-08-16 14:29:15 -0400

    quiz is done

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
-  //example code - feel free to delete!
-  var timesSillyButtonClicked = 0;
-  $('#silly-button').click(function() {
-    timesSillyButtonClicked++;
-    $('#silly-button-text').text(
-      'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
-    );
-  });
 
-  //write your code here!
-});
+
+
+      $("#femyes1").click(function() {
+     $("#result").text("Actually, most femenists don't have problems with men. The whole point of femenism is to promote equality between genders.");
+
+      });
+
+      $("#femno1").click(function() {
+        $("#result1").text("correct!");
+
+         });
+
+        $("#femyes2").click(function() {
+          $("#result2").text("Correct, Trevor Noah, Will Smith, and so many other men are femenists.");
+
+        });
+
+        $("#femno2").click(function() {
+          $("#result").text("Actually, Trevor Noah, Will Smith, and so many other powerful and influential men are femenists.");
+
+           });
+
+          $("#femyes3").click(function() {
+            $("#result3").text("During the Women's Liberation Movement, or any true femenist movemnt, no women burned bras. The idea of women burning bras was a scheme to discredit the ideas and points of femenism. And by clicking this yes, that shows it worked");
+
+             });
+
+
+          $("#femno3").click(function() {
+          $("#result4").text("Okay. You know your stuff.");
+
+           });
+         });
