8af76dbd32ea8c3b69171b0ff2310e8c1a3d64f5
MHC User <mhcuser@n21-62.mtholyoke.edu>
2017-08-17 15:41:58 -0400

    almost done

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
+   // (confirm("I will agree to the I Can Pledge as shown below") === true)
+
 $(document).ready(function() {
-    // (confirm("I will agree to the I Can Pledge as shown below") === true)
+
+
+      $("#everything").hide();
+
+
+  $("#confirm").click(function() {
+    console.log("push")
+      $("#everything").fadeIn();
+        $("#confirm").fadeOut();
+  });
+
+
+
 
 
       $("#femyes1").click(function() {
-     $("#result").text("Actually, most femenists don't have problems with men. The whole point of femenism is to promote equality between genders.");
+        console.log("hello")
+     $("#result").text("Actually, most feminists don't have problems with men. The whole point of feminism is to promote equality between genders.");
 
       });
 
@@ -14,17 +29,17 @@ $(document).ready(function() {
          });
 
         $("#femyes2").click(function() {
-          $("#result2").text("Correct, Trevor Noah, Will Smith, and so many other men are femenists.");
+          $("#result2").text("Correct, Trevor Noah, Will Smith, and so many other men are feminists.");
 
         });
 
         $("#femno2").click(function() {
-          $("#result").text("Actually, Trevor Noah, Will Smith, and so many other powerful and influential men are femenists.");
+          $("#result").text("Actually, Trevor Noah, Will Smith, and so many other powerful and influential men are feminists.");
 
            });
 
           $("#femyes3").click(function() {
-            $("#result3").text("During the Women's Liberation Movement, or any true femenist movemnt, no women burned bras. The idea of women burning bras was a scheme to discredit the ideas and points of femenism. And by clicking this yes, that shows it worked");
+            $("#result3").text("During the Women's Liberation Movement, or any true feminist movemnt, no women burned bras. The idea of women burning bras was a scheme to discredit the ideas and points of feminism. And by clicking this yes, that shows it worked.");
 
              });
 
@@ -33,4 +48,4 @@ $(document).ready(function() {
           $("#result4").text("Okay. You know your stuff.");
 
            });
-         });
+ });
